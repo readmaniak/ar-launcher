789c613205ce2cf885889cf350fb45081c1f7070
ReadManiak <read.maniac@yandex.ru>
2023-02-06 09:55:17 +0300

    steam connector

---
+++ ARLauncher/ARLauncher/Program.cs
@@ -0,0 +1,5 @@
+﻿using ARLauncher;
+
+var ticket = await SteamConnector.GetSteamTicket();
+
+SteamConnector.Shutdown();
---
+++ ARLauncher/ARLauncher/SteamConnector.cs
@@ -0,0 +1,27 @@
+﻿using Steamworks;
+using System.Text;
+
+namespace ARLauncher
+{
+    internal static class SteamConnector
+    {
+        public static async Task<string> GetSteamTicket()
+        {
+            SteamClient.Init(480);
+            var ticket = await SteamUser.GetAuthSessionTicketAsync();
+            if (ticket == null)
+            {
+                throw new Exception("Failed to get SessionTicket");
+            }
+            var array = ticket.Data;
+            var sb = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                sb.Append(string.Format("{0:X2}", array[i]));
+            }
+            return sb.ToString();
+        }
+
+        public static void Shutdown() => SteamClient.Shutdown();
+    }
+}
